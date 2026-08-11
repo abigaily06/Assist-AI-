fcb94de5b5e489cbf714eccf7a9c8a81c242b815
Miya <miyamor@users.noreply.huggingface.co>
2026-08-11 15:33:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ from huggingface_hub import InferenceClient
 
 client = InferenceClient("Qwen/Qwen2.5-7B-Instruct", bill_to="kode-with-klossy")
 
-with open("assist_ai_dataset1.txt", "r", encoding="utf-8") as file:
-        assist_ai_dataset1_text = file.read()
-    print(assist_ai_dataset1_text)
+with open("assist_ai_database1.txt", "r", encoding="utf-8") as file:
+        assist_ai_database1_text = file.read()
+    print(assist_ai_database1_text)
 
 
 
